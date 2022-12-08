a9a4e90aaad339c4bd6b537f80edb2e39e6cc0b7
NahuelPicca <nahuelpicca@gmail.com>
2022-12-08 00:01:08 -0300

    Fix: se agregaron interfaces de dichas clases.

--- RossiEventos/RossiEventos/Entidades/Producto.cs
+++ RossiEventos/RossiEventos/Entidades/Producto.cs
@@ -2,6 +2,7 @@
 using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
 using Microsoft.EntityFrameworkCore;
+using RossiEventos.Interfaces;
 
 namespace RossiEventos.Entidades
 {
