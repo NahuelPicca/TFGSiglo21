cc857e59c8f3ab60ae3a121205344b329e34fd9c
NahuelPicca <nahuelpicca@gmail.com>
2023-01-02 14:54:21 -0300

    Feat: se constituyó la clase UsuarioControllers.

--- RossiEventos/RossiEventos/Controllers/UsuarioController.cs
+++ RossiEventos/RossiEventos/Controllers/UsuarioController.cs
@@ -25,9 +25,11 @@ namespace RossiEventos.Controllers
         [HttpGet("{id:int}")]
         public async Task<ActionResult<UsuarioDto>> GetUsuario(int id)
         {
-            logger.LogInformation("Lista de usuarios");
+            logger.LogInformation("Get usuario");
             var usuario = await context.Usuario.FirstOrDefaultAsync(u => u.Id == id);
-            return mapper.Map<UsuarioDto>(usuario);
+            if (usuario != null)
+                return mapper.Map<UsuarioDto>(usuario);
+            return NotFound($"No se encontró el usuario con el Id: {id}");
         }
 
         [HttpGet()]
@@ -38,12 +40,27 @@ namespace RossiEventos.Controllers
             return mapper.Map<List<UsuarioDto>>(listUsuario);
         }
 
-        //[HttpPost()]
-        //public async Task<ActionResult<List<UsuarioDto>>> GetListUsuario(int id)
-        //{
-        //    logger.LogInformation("Lista de usuarios");
-        //    var listUsuario = await context.Usuario.ToListAsync();
-        //    return mapper.Map<List<UsuarioDto>>(listUsuario);
-        //}
+        [HttpPost()]
+        public async Task<ActionResult> PostUsuarioDto([FromBody] UsuarioDto usuarioDto)
+        {
+            var usuario = mapper.Map<Usuario>(usuarioDto);
+            context.Add(usuario);
+            var aa = await context.SaveChangesAsync();
+            return Ok(aa);
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult> DeleteUsuario(int id)
+        {
+            var usuario = await context.Usuario
+                                       .FirstOrDefaultAsync(u => u.Id == id);
+            if (usuario != null)
+            {
+                context.Usuario.Remove(usuario);
+                var aa = context.SaveChanges();
+                return Ok(aa);
+            }
+            return NotFound($"No se encontró el Usuario con el Id: {id}");
+        }
     }
 }
