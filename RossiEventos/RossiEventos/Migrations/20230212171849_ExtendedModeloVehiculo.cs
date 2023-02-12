064ee6e869fd20f7c5e3c9541ce5d392f8e62025
NahuelPicca <nahuelpicca@gmail.com>
2023-02-12 14:29:13 -0300

    Fix: se agregó una migración para agrandar el largo de la propiedad Modelo de Vehiculo.

---
+++ RossiEventos/RossiEventos/Migrations/20230212171849_ExtendedModeloVehiculo.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RossiEventos.Migrations
+{
+    /// <inheritdoc />
+    public partial class ExtendedModeloVehiculo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Modelo",
+                table: "Vehiculo",
+                type: "varchar(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(10)",
+                oldMaxLength: 10);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Modelo",
+                table: "Vehiculo",
+                type: "varchar(10)",
+                maxLength: 10,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(100)",
+                oldMaxLength: 100);
+        }
+    }
+}
