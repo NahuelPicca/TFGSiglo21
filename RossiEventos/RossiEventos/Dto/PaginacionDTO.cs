73ee5769333adb5b00428ee6094c089742fed305
NahuelPicca <nahuelpicca@gmail.com>
2024-04-24 21:02:09 -0300

    Fix: se agregó creó un nuevo DTO para páginas.

---
+++ RossiEventos/RossiEventos/Dto/PaginacionDTO.cs
@@ -0,0 +1,18 @@
+﻿namespace PeliculasAPI.Dto
+{
+    public class PaginacionDTO
+    {
+        private int recordsPorPagina = 10;
+        private readonly int cantMaxRecordsPorPag = 50;
+        public int PagActual { get; set; } = 1;
+
+        public int RecordsPorPagina
+        {
+            get { return recordsPorPagina; }
+            set
+            {
+                recordsPorPagina = (value > cantMaxRecordsPorPag) ? cantMaxRecordsPorPag : value;
+            }
+        }
+    }
+}
