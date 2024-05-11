d17b1b2fa5fc990ef1cf1919c6016dbf6f1b7abe
NahuelPicca <nahuelpicca@gmail.com>
2024-05-11 11:36:15 -0300

    Fix: se adaptó route-config.

--- RossiEventosFront/src/route-config.ts
+++ RossiEventosFront/src/route-config.ts
@@ -1,15 +1,16 @@
 //import { LandingPage } from "./LandingPage";
 import RedireccionarALanding from "./RedireccionarALanding";
 // import Registro from "./auth/Registro";
-// import Login from "./auth/Login";
+ import Login from "./auth/Login";
+import Registro from "./auth/Registro";
 // import IndiceUsuarios from "./auth/IndiceUsuarios";
 
 //:id(\\d+) Se usa para poder recibir en la ruta relativa un ID de tipo
 //int como parámetro
 //Ejemplo /actores/editar/:id
 const rutas = [
-    // {path: '/registro', componente: Registro},
-    // {path: '/login', componente: Login},
+     {path: '/registro', componente: Registro},
+     {path: '/login', componente: Login},
     // {path: '/usuarios', componente: IndiceUsuarios, esAdmin: true},
 
     // {path: '/', componente: LandingPage, exact: true},
