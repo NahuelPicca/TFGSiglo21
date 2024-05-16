a2e9a0e6f1f823850574d03c550ac133a5ccca0c
NahuelPicca <nahuelpicca@gmail.com>
2024-05-16 00:16:36 -0300

    Fix: se creó la clase CredencialesUsuarioDto para el registro del usuario.

---
+++ RossiEventos/RossiEventos/Dto/CredencialesUsuarioDTO.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RossiEventos.Dto
+{
+    public class CredencialesUsuarioDTO
+    {
+        [Required]
+        public string Nombre { get; set; }
+
+        [Required]
+        public string Apellido { get; set; }
+
+        [Required]
+        public string Cuit { get; set; }
+
+        [Required]
+        public string NroDni { get; set; }
+
+        [Required]
+        public string CodigoPostal { get; set; }
+
+        [Required]
+        public string Direccion { get; set; }
+
+        [Required]
+        public string Localidad { get; set; }
+        [Required]
+        public string Telefono { get; set; }
+
+        [EmailAddress]
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string Contraseña { get; set; }
+
+        [Required]
+        public DateTime FechaNacimiento { get; set; }
+    }
+}
