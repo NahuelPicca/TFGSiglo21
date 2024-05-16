4a0fc852e357bf5d47a9d0a25c9f7c8ad4263ef9
NahuelPicca <nahuelpicca@gmail.com>
2024-05-16 00:17:41 -0300

    Fix: se agregó la clase RespuestaAutenticacionDto para que devuelva el token y fecha de expiracion.

---
+++ RossiEventos/RossiEventos/Dto/RespuestaAutenticacionDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace RossiEventos.Dto
+{
+    public class RespuestaAutenticacionDTO
+    {
+        public string Token { get; set; }
+        public DateTime Expiracion { get; set; }
+    }
+}
