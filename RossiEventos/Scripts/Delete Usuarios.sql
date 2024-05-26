803c807dcb34ccac990a606fb90faa6b2c12e3c8
NahuelPicca <nahuelpicca@gmail.com>
2024-05-26 18:21:20 -0300

    Fix: se agregó una carpeta con scripts.

---
+++ RossiEventos/Scripts/Delete Usuarios.sql
@@ -0,0 +1,10 @@
+select * from Usuario
+select * from Persona
+
+select * from AspNetUsers
+select * from AspNetRoleClaims
+
+
+delete Usuario where id=37
+delete Persona where id=37
+delete AspNetUsers where UserName='mpiccaale@gmail.com'
