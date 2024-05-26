fb4c21b6365aa0c7610cedcf6199774a33abf34e
NahuelPicca <nahuelpicca@gmail.com>
2024-05-26 18:19:56 -0300

    Fix: se creó el objeto LogeoUsuarioDto.

---
+++ RossiEventos/RossiEventos/Dto/LogeoUsuarioDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RossiEventos.Dto
+{
+    public class LogeoUsuarioDto
+    {
+        [EmailAddress]
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+    }
+}
