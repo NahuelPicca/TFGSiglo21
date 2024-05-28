d6f38c8c0fa9e0a2afbd941b6b2e0dc5233fd2b8
NahuelPicca <nahuelpicca@gmail.com>
2024-05-27 22:14:49 -0300

    Other: se cambió una ruta.

--- RossiEventosFront/src/App.tsx
+++ RossiEventosFront/src/App.tsx
@@ -7,7 +7,7 @@ import {
   BrowserRouter
 } from "react-router-dom";
 import rutas from './route-config';
-import validaciones from './validaciones';
+import validaciones from './validaciones/validaciones';
 
 validaciones();
 
