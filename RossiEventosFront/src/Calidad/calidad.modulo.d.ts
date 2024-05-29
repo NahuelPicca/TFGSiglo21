b36394c3dd95aae65c1eb59cf69d4b07890935ea
NahuelPicca <nahuelpicca@gmail.com>
2024-05-28 22:08:26 -0300

    Fix: se creó el modelo de calidad.

---
+++ RossiEventosFront/src/Calidad/calidad.modulo.d.ts
@@ -0,0 +1,12 @@
+export interface calidadDTO{
+    id: number;
+    codigo: string;
+    nombre: string;
+    descripcion: string;
+}
+
+export interface calidadCreacionDTO{
+    codigo: string;
+    nombre: string;
+    descripcion: string;
+}
