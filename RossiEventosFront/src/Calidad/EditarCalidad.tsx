170398e929e90af9cf64452ec2ac4b1338399e56
NahuelPicca <nahuelpicca@gmail.com>
2024-05-28 22:08:58 -0300

    Fix: se creó la clase EditarCalidad.

---
+++ RossiEventosFront/src/Calidad/EditarCalidad.tsx
@@ -0,0 +1,19 @@
+import EditarEntidad from "../utils/EditarEntidades";
+import { urlCalidad } from "../utils/endpoints";
+import FormularioCalidad from "./FormularioCalidad";
+import { calidadCreacionDTO, calidadDTO } from "./calidad.modulo";
+
+export default function EditarCines() {
+    return (
+        <EditarEntidad<calidadCreacionDTO, calidadDTO>
+            url={urlCalidad} urlIndice="/calidad" nombreEntidad="Calidad"
+        >
+            {(entidad, editar) =>
+                <FormularioCalidad modelo={entidad}
+                    onSubmit={async valores => {
+                        await
+                            editar(valores);
+                    }} />}
+        </EditarEntidad>
+    )
+}
