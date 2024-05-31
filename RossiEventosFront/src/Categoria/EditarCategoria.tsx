1f6ebbc48d183200c0934074ebca644043b60930
NahuelPicca <nahuelpicca@gmail.com>
2024-05-30 23:14:22 -0300

    Fix: se creó el formulario EditarCategoría.

---
+++ RossiEventosFront/src/Categoria/EditarCategoria.tsx
@@ -0,0 +1,19 @@
+import EditarEntidad from "../utils/EditarEntidades";
+import { urlCategoria } from "../utils/endpoints";
+import FormularioCategoria from "./FormularioCategoria";
+import { categoriaDTO, creacionCategoriaDTO } from "./categoria.modulo";
+
+export default function EditarCategoria() {
+    return (
+        <EditarEntidad<creacionCategoriaDTO, categoriaDTO>
+            url={urlCategoria} urlIndice="/categoria" nombreEntidad="Categoria"
+        >
+            {(entidad, editar) =>
+                <FormularioCategoria modelo={entidad}
+                    onSubmit={async valores => {
+                        await
+                            editar(valores);
+                    }} />}
+        </EditarEntidad>
+    )
+}
