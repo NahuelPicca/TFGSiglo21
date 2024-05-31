52268e95abfe56c61ac34a140785420077b0318b
NahuelPicca <nahuelpicca@gmail.com>
2024-05-30 23:13:58 -0300

    Fix: se creó el formulario de CrearCategoria.

---
+++ RossiEventosFront/src/Categoria/CrearCategoria.tsx
@@ -0,0 +1,34 @@
+import axios from "axios";
+import { useState } from "react";
+import MostrarErrores from "../utils/MostrarErrores";
+import { useNavigate } from "react-router-dom";
+import { urlCategoria } from "../utils/endpoints";
+import { creacionCategoriaDTO } from "./categoria.modulo";
+import FormularioCategoria from "./FormularioCategoria";
+
+export default function CrearCategoria() {
+
+    const [errores, setErrores] = useState<string[]>([]);
+    const navigate = useNavigate();
+    async function creaCategoria(categoria: creacionCategoriaDTO) {
+        try {
+            await axios.post(`${urlCategoria}/crear`, categoria)
+            navigate("/");
+        } catch (error: any) {
+            setErrores(error.response.data)
+        }
+    }
+
+    return (
+        <>
+            <h3>Nueva categoría</h3>
+            <MostrarErrores errores={errores} />
+            <FormularioCategoria
+                modelo={{
+                    nombre: '',
+                    descripcion: '',
+                }}
+                onSubmit={async valores => await creaCategoria(valores)} />
+        </>
+    )
+}
