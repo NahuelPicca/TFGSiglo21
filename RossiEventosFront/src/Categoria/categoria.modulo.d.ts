70dfcd74969f0b97ec427ddaaa4ba2727c7660c2
NahuelPicca <nahuelpicca@gmail.com>
2024-05-30 23:13:29 -0300

    Fix: se agregó el módulo de categoría.

---
+++ RossiEventosFront/src/Categoria/categoria.modulo.d.ts
@@ -0,0 +1,10 @@
+export interface categoriaDTO{
+    id:number;
+    nombre: string;
+    descripcion: string;
+}
+
+export interface creacionCategoriaDTO{
+    nombre: string;
+    descripcion: string;
+}
