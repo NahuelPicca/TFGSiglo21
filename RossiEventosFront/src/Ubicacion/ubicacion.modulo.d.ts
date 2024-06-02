c9aa14d9ffcaac78089c1b674d0899b498328824
NahuelPicca <nahuelpicca@gmail.com>
2024-06-02 18:59:34 -0300

    Fix: se creó el módulo de ubicacion.

---
+++ RossiEventosFront/src/Ubicacion/ubicacion.modulo.d.ts
@@ -0,0 +1,18 @@
+export interface ubicacionDTO{
+    id:number;
+    codigo: string;
+    nombre: string;
+    columna: string;
+    estante: string;
+    fila: string;
+    habilitado: boolean;
+}
+
+export interface creacionUbicacionDTO{
+    codigo: string;
+    nombre: string;
+    columna: string;
+    estante: string;
+    fila: string;
+    habilitado: boolean;
+}
