8a8c2a68513ae9d03ecdcb5d69932dbaf3e69c71
NahuelPicca <nahuelpicca@gmail.com>
2024-06-09 15:10:35 -0300

    Fix: se creó el LandingPage.

---
+++ RossiEventosFront/src/LandingPage/LandingPage.tsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+import Menu from "../utils/Menu";
+
+export function LandingPage() {
+    return (
+        //<Menu />
+        <h1>aaaaaaaaa</h1>
+    )
+}
+
+
+
