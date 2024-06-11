7ac4106ea70b74700da96f002715ab720c897f62
NahuelPicca <nahuelpicca@gmail.com>
2024-06-11 15:46:28 -0300

    Fix: se adaptó el objeto del modelo de producto.

--- RossiEventosFront/src/Productos/producto.modulo.d.ts
+++ RossiEventosFront/src/Productos/producto.modulo.d.ts
@@ -3,10 +3,13 @@ export interface productoDTO{
     codigo: string;
     descripcion: string;
     marca: string;
+    poster1: string;
+    poster2: string;
+    poster3: string;
     anio: Date;
     habilitado: bool;
     calidadId: int;
-    tipoId: int;
+    tipoProductoId: int;
     precio: number;
 }
 
@@ -14,9 +17,15 @@ export interface creacionProductoDTO{
     codigo: string;
     descripcion: string;
     marca: string;
+    poster1?: File;
+    poster2?: File;
+    poster3?: File;
+    posterURL1?: string;
+    posterURL2?: string;
+    posterURL3?: string;
     anio?: Date;
     habilitado: bool;
     calidadId: int;
-    tipoId: int;
+    tipoProductoId: int;
     precio: number;
 }
