bf3cb13c6ddef9d8a4203dca8225427c2546e3fc
NahuelPicca <nahuelpicca@gmail.com>
2024-06-11 15:34:59 -0300

    Fix: se adaptó el formulario CrearProducto.

--- RossiEventosFront/src/Productos/CrearProducto.tsx
+++ RossiEventosFront/src/Productos/CrearProducto.tsx
@@ -1,10 +1,12 @@
-import axios from "axios";
+import axios, { AxiosResponse } from "axios";
 import { useState } from "react";
 import MostrarErrores from "../utils/MostrarErrores";
 import { useNavigate } from "react-router-dom";
 import { urlProducto } from "../utils/endpoints";
 import FormularioProducto from "./FormularioProducto";
 import { creacionProductoDTO } from "./producto.modulo";
+import { convertirProductoAFormData } from "./baseFormDataProducto";
+//import { convertirPeliculaAFormData } from "../utils/formDataUtils";
 
 export default function CrearProducto() {
 
@@ -19,6 +21,22 @@ export default function CrearProducto() {
         }
     }
 
+    async function crear(producto: creacionProductoDTO) {
+        try {
+            const formData = convertirProductoAFormData(producto);
+            await axios({
+                method: 'post',
+                url: `${urlProducto}/crear`,
+                data: formData,
+                headers: { 'Content-Type': 'multipart/form-data' }
+            }).then((respuesta: AxiosResponse<number>) => {
+                navigate(`/producto/${respuesta.data}`);
+            })
+        } catch (error: any) {
+            setErrores(error.response.data);
+        }
+    }
+
     return (
         <>
             <h3>Nuevo producto</h3>
@@ -29,11 +47,52 @@ export default function CrearProducto() {
                     descripcion: '',
                     marca: '',
                     calidadId: 0,
-                    tipoId: 0,
+                    tipoProductoId: 0,
                     precio: 0,
                     habilitado: true
                 }}
-                onSubmit={async valores => await creaProducto(valores)} />
+                onSubmit={async valores => await crear(valores)} />
         </>
     )
 }
+
+// export function convertirProductoAFormData(producto: creacionProductoDTO): FormData {
+//     const formData = new FormData();
+//     formData.append('codigo', producto.codigo);
+//     formData.append('descripcion', producto.descripcion);
+//     formData.append('marca', producto.marca);
+
+//     if (producto.poster1) {
+//         formData.append("poster1", producto.poster1);
+//     }
+//     if (producto.poster2) {
+//         formData.append("poster2", producto.poster2);
+//     }
+//     if (producto.poster3) {
+//         formData.append("poster3", producto.poster3);
+//     }
+//     if (producto.anio) {
+//         formData.append('anio', formatearFecha(producto.anio));
+//     }
+//     formData.append("calidadId", JSON.stringify(producto.calidadId));
+//     formData.append("tipoId", JSON.stringify(producto.tipoId));
+//     formData.append("precio", JSON.stringify(producto.precio));
+//     return formData;
+// }
+
+// export function formatearFecha(date: Date) {
+//     date = new Date(date);
+//     const formato = new Intl.DateTimeFormat("en", {
+//         year: 'numeric',
+//         month: '2-digit',
+//         day: '2-digit'
+//     });
+
+//     const [
+//         { value: month }, ,
+//         { value: day }, ,
+//         { value: year }
+//     ] = formato.formatToParts(date);
+
+//     return `${year}-${month}-${day}`
+// }
