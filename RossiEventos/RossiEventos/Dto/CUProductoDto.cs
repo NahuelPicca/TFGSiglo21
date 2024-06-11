8d4c30ee8f62080d839e9d194f099bb8180da0ad
NahuelPicca <nahuelpicca@gmail.com>
2024-06-11 15:16:48 -0300

    Fix: se adaptó CUProductoDto.

--- RossiEventos/RossiEventos/Dto/CUProductoDto.cs
+++ RossiEventos/RossiEventos/Dto/CUProductoDto.cs
@@ -3,19 +3,15 @@
     public class CUProductoDto 
     {
         public string Codigo { get; set; }
-
         public string Descripcion { get; set; }
-
         public string Marca { get; set; }
-
         public DateTime Anio { get; set; }
-
         public bool Habilitado { get; set; }
         public decimal Precio { get; set; }
-
         public int CalidadId { get; set; }
-
         public int TipoProductoId { get; set; }
-
+        public IFormFile? Poster1 { get; set; }
+        public IFormFile? Poster2 { get; set; }
+        public IFormFile? Poster3 { get; set; }
     }
 }
