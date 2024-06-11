6f0faa9877054f203cc4a5a308cde95d41ca17e1
NahuelPicca <nahuelpicca@gmail.com>
2024-06-11 00:21:10 -0300

    Fix: se agregó una migración para agrandar a MAX los campos posters de productos.

---
+++ RossiEventos/RossiEventos/Migrations/20240611031837_ActualizaCamposPosterAMaxEnProducto.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RossiEventos.Migrations
+{
+    /// <inheritdoc />
+    public partial class ActualizaCamposPosterAMaxEnProducto : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Poster3",
+                table: "Producto",
+                type: "varchar(MAX)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Poster2",
+                table: "Producto",
+                type: "varchar(MAX)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Poster1",
+                table: "Producto",
+                type: "varchar(MAX)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Poster3",
+                table: "Producto",
+                type: "varchar",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(MAX)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Poster2",
+                table: "Producto",
+                type: "varchar",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(MAX)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Poster1",
+                table: "Producto",
+                type: "varchar",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(MAX)");
+        }
+    }
+}
