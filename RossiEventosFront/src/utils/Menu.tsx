abb7b5ce73340f2bfa1489984f1e7c586bcc8987
NahuelPicca <nahuelpicca@gmail.com>
2024-06-17 14:16:38 -0300

    Fix: se adaptó el menú.

--- RossiEventosFront/src/utils/Menu.tsx
+++ RossiEventosFront/src/utils/Menu.tsx
@@ -6,157 +6,86 @@ import Button from "./Button";
 //import AutenticacionContext from "../auth/AutenticacionContext";
 
 export default function Menu() {
-    const navegar = useNavigate();
-    //const claseActiva = 'active';
-    //const { actualizar, claims } = useContext(AutenticacionContext);
+  const navegar = useNavigate();
+  //const claseActiva = 'active';
+  //const { actualizar, claims } = useContext(AutenticacionContext);
 
-    // function obtenerNombreUsuario(): string {
-    //     return claims.filter(x => x.nombre === "Email")[0]?.valor;
-    // }
+  // function obtenerNombreUsuario(): string {
+  //     return claims.filter(x => x.nombre === "Email")[0]?.valor;
+  // }
 
-    function obtenerNombreUsuario(logueado: boolean): void {
-        logueado ? navegar('/Login') : navegar('/Registro')
-    }
+  function obtenerNombreUsuario(logueado: boolean): void {
+    logueado ? navegar('/Login') : navegar('/Registro')
+  }
 
-    return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light" >
-            <div className="container-fluid">
-                <NavLink className='navbar-brand' to="/">Rossi Eventos</NavLink>
-                <div className="collapse navbar-collapse"
-                    style={{ display: 'flex', justifyContent: 'space-between' }}>
-                    <ul className="nav justify-content-end">
-                        <li className="nav-item">
-                            <NavLink className='nav-link'
-                                to='/reservas'>
-                                Reservas
-                            </NavLink>
-                        </li>
-                        {/* <Autorizado role="admin" */}
-                        {/* autorizado={ */}
-                        <li className="nav-item">
-                            <NavLink className='nav-link' to='/pedidos'>
-                                {/* ClassName de NavLink lo dejamos asi por las dudas, pero ese condicional no iría o hay que cambiarlo */}
-                                Pedidos
-                            </NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink className={({ isActive }) => { return isActive ? 'nav-link' : 'nav-link' }}
-                                to='/stock'>
-                                Stock
-                            </NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink className={({ isActive }) => { return isActive ? 'nav-link' : 'nav-link' }}
-                                to='/productos'>
-                                Productos
-                            </NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink className={({ isActive }) => { return isActive ? 'nav-link' : 'nav-link' }}
-                                to='/gestion'>
-                                Gestión
-                            </NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink className={({ isActive }) => { return isActive ? 'nav-link' : 'nav-link' }}
-                                to='/contacto'>
-                                Contacto
-                            </NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink className={({ isActive }) => { return isActive ? 'nav-link' : 'nav-link' }}
-                                to='/calidad'>
-                                Calidad
-                            </NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink className={({ isActive }) => { return isActive ? 'nav-link' : 'nav-link' }}
-                                to='/deposito'>
-                                Depositos
-                            </NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink className={({ isActive }) => { return isActive ? 'nav-link' : 'nav-link' }}
-                                to='/categoria'>
-                                Categoría
-                            </NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink className={({ isActive }) => { return isActive ? 'nav-link' : 'nav-link' }}
-                                to='/ubicacion'>
-                                Ubicaciones
-                            </NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink className={({ isActive }) => { return isActive ? 'nav-link' : 'nav-link' }}
-                                to='/producto'>
-                                Productos
-                            </NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink className={({ isActive }) => { return isActive ? 'nav-link' : 'nav-link' }}
-                                to='/tipoProducto'>
-                                Tipos de Productos
-                            </NavLink>
-                        </li>
-                        {/* } /> */}
-                    </ul>
-                    <div className="d-flex">
-                        {/* <Autorizado
-                 autorizado={ */}
-                        <>
-                            {/* <span className="nav-link">Hola, {obtenerNombreUsuario()}</span>
-                 <Button className='nav-link btn btn-link'
-                     onClick={() => {
-                         logout();
-                         actualizar([]);
-                     }}>Log out</Button> */}
-                            {/* </>} */}
-                            {/* noAutorizado={ */}
-                            <div className="d-flex">
-                                <>
-                                    <Button className="nav-link btn btn-link"
-                                        onClick={() => {
-                                            obtenerNombreUsuario(false)
-                                        }}>
-                                        Registro
-                                    </Button>
+  return (
+    <div className="container">
+      <nav className="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3">
+        <div className="col-md-3 mb-2 mb-md-0">
+          <a href="/" className="d-inline-flex link-body-emphasis text-decoration-none">
+            <img src="https://startbootstrap.com/assets/img/sb-logo.svg" width="100" height="50"></img>
+          </a>
+        </div>
 
-                                    <Button className="nav-link btn btn-link"
-                                        onClick={() => {
-                                            obtenerNombreUsuario(true)
-                                        }}>
-                                        Inicio de Sessión
-                                    </Button>
-                                </>
-                            </div>
-                            {/* } */}
-                            {/* /> */}
-                        </>
-                    </div>
-                </div>
-            </div>
-        </nav>
-    )
-    // return (
-    //     <>
-    //         <ul className="nav justify-content-end">
-    //             <li className="nav-item">
-    //                 <a className="nav-link active" href="#">Active</a>
-    //             </li>
-    //             <li className="nav-item">
-    //                 <a className="nav-link" href="#">Link</a>
-    //             </li>
-    //             <li className="nav-item">
-    //                 <a className="nav-link" href="#">Link</a>
-    //             </li>
-    //             <li className="nav-item">
-    //                 <a className="nav-link disabled" href="#">Disabled</a>
-    //             </li>
-    //         </ul>
-    //     </>
+        <ul className="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+          <li><a href="/" className="nav-link px-2 link-secondary">Home</a></li>
+          <li><a href="/reservas" className="nav-link px-2">Reservas</a></li>
+          <li><a href="/pedidos" className="nav-link px-2">Pedidos</a></li>
 
-    // )
+
+          <li className="nav-item dropdown">
+            <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Stock
+            </a>
+            <ul className="dropdown-menu">
+              <li><a className="dropdown-item" href="/deposito">Depositos</a></li>
+              <li><a className="dropdown-item" href="/ubicacion">Ubicación</a></li>
+              <li><a className="dropdown-item" href="#">Movimiento de Stock</a></li>
+              <li><a className="dropdown-item" href="/producto">Productos</a></li>
+              <li><a className="dropdown-item" href="#">Saldos Ubicación</a></li>
+            </ul>
+          </li>
+          <li className="nav-item dropdown">
+            <a className="nav-link dropdown-toggle" href="/gestion" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Gestión
+            </a>
+            <ul className="dropdown-menu">
+              <li><a className="dropdown-item" href="/calidad">Calidad</a></li>
+              <li><a className="dropdown-item" href="/tipoProducto">Tipo de producto</a></li>
+              <li><a className="dropdown-item" href="/categoria">Categoría</a></li>
+              <li><a className="dropdown-item" href="#">Vehiculos</a></li>
+              <li><a className="dropdown-item" href="/transportista">Transportista</a></li>
+              <li><a className="dropdown-item" href="#">Asignar Vehiculos</a></li>
+              <li><a className="dropdown-item" href="/usuario">Usuarios</a></li>
+            </ul>
+          </li>
+        </ul>
+
+        <div className="col-md-3 text-end">
+          <a href="/Login" className="btn btn-outline-primary me-2" role="button" aria-pressed="true">Iniciar Sesión</a>
+          <a href="/registro" className="btn btn-primary" role="button" aria-pressed="true">Registro</a>
+        </div>
+      </nav>
+    </div>
+  )
+  // return (
+  //     <>
+  //         <ul classNameNameNameNameName="nav justify-content-end">
+  //             <li classNameNameNameNameName="nav-item">
+  //                 <a classNameNameNameNameName="nav-link active" href="#">Active</a>
+  //             </li>
+  //             <li classNameNameNameNameName="nav-item">
+  //                 <a classNameNameNameNameName="nav-link" href="#">Link</a>
+  //             </li>
+  //             <li classNameNameNameNameName="nav-item">
+  //                 <a classNameNameNameNameName="nav-link" href="#">Link</a>
+  //             </li>
+  //             <li classNameNameNameNameName="nav-item">
+  //                 <a classNameNameNameNameName="nav-link disabled" href="#">Disabled</a>
+  //             </li>
+  //         </ul>
+  //     </>
+
+  // )
 }
-
-//activeClassName = {}
+//activeclassNameNameNameNameName = {}
