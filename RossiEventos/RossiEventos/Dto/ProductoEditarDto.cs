ee76a26f8b3f59f5daa6d31e3861684d0664ea33
NahuelPicca <nahuelpicca@gmail.com>
2024-07-02 00:07:55 -0300

    Fix: se agregó la clase ProductoEditarDto.

---
+++ RossiEventos/RossiEventos/Dto/ProductoEditarDto.cs
@@ -0,0 +1,19 @@
+﻿namespace RossiEventos.Dto
+{
+    public class ProductoEditarDto
+    {
+        public string Id { get; set; }
+        public string Codigo { get; set; }
+        public string Descripcion { get; set; }
+        public string Marca { get; set; }
+        public string Poster1 { get; set; }
+        public string Poster2 { get; set; }
+        public string Poster3 { get; set;}
+        public DateTime Anio { get; set;}
+        public bool Habilitado { get; set; }
+        public int CalidadId { get; set; }
+        public string CodigoCalidad { get; set; }
+        public int TipoProductoId { get; set; }
+        public decimal Precio { get; set; }
+    }
+}
