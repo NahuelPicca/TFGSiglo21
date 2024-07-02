38353fdc0fd6df6a73cfb400a4652a5bb8b03a51
NahuelPicca <nahuelpicca@gmail.com>
2024-07-02 00:09:29 -0300

    Fix: se había creado una migración para que ANIO sea de tipo DATE.

---
+++ RossiEventos/RossiEventos/Migrations/20240625032401_CambiaFormatoFechaProducto.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RossiEventos.Migrations
+{
+    /// <inheritdoc />
+    public partial class CambiaFormatoFechaProducto : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Anio",
+                table: "Producto",
+                type: "Date",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Anio",
+                table: "Producto",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "Date");
+        }
+    }
+}
