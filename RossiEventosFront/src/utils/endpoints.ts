3a38017711815eea8ae5acc7da38b7752eef4c5a
NahuelPicca <nahuelpicca@gmail.com>
2024-07-10 11:19:40 -0300

    Fix: se adaptó el endpoints.

--- RossiEventosFront/src/utils/endpoints.ts
+++ RossiEventosFront/src/utils/endpoints.ts
@@ -1,13 +1,16 @@
 const apiURL = import.meta.env.VITE_APP_API_URL
-export const urlUsuarios = `${apiURL}/usuarios`
+
 export const urlCalidad = `${apiURL}/calidad`
-export const urlDeposito = `${apiURL}/deposito`
 export const urlCategoria = `${apiURL}/categoria`
-export const urlUbicacion = `${apiURL}/ubicacion`
+export const urlDeposito = `${apiURL}/deposito`
 export const urlMovimiento = `${apiURL}/movimiento`
 export const urlProducto = `${apiURL}/producto`
 export const urlTipoProducto = `${apiURL}/tipoProducto`
+export const urlTitular = `${apiURL}/titulares`
 export const urlTransportista = `${apiURL}/transportista`
+export const urlUbicacion = `${apiURL}/ubicacion`
+export const urlUsuario = `${apiURL}/usuarios`
 export const urlVehiculo = `${apiURL}/vehiculos`
+
 // export const urlCuentas = `${apiURL}/cuentas`
 // export const urlRatings = `${apiURL}/rating`
