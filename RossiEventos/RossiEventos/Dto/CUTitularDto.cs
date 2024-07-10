242d809b4988083cc8d1f315672950551b409eb3
NahuelPicca <nahuelpicca@gmail.com>
2024-07-10 11:13:59 -0300

    Fix: se adaptó la clase CUTitularDto.

--- RossiEventos/RossiEventos/Dto/CUTitularDto.cs
+++ RossiEventos/RossiEventos/Dto/CUTitularDto.cs
@@ -2,6 +2,7 @@
 {
     public class CUTitularDto
     {
+        public int Id { get; set; }
         public string Nombre { get; set; }
         public string Apellido { get; set; }
         public string TipoDni { get; set; }
