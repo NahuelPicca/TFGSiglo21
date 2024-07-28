6a5c6d02e865ce58c35f5e2532aa78bfa5eec7e4
NahuelPicca <nahuelpicca@gmail.com>
2024-07-28 16:09:36 -0300

    Fix: se adaptó el landing page.

--- RossiEventosFront/src/LandingPage/LandingPage.tsx
+++ RossiEventosFront/src/LandingPage/LandingPage.tsx
@@ -1,31 +1,31 @@
 export function LandingPage() {
-    return (
-        <div className="hero-container" id="hero-sec">
-        <div className="container-fluid ">
+  return (
+    <div className="hero-container" id="hero-sec">
+      <div className="container-fluid ">
         <div className="row d-flex">
           <div className="col align-middle">
             <div className="px-2 py-2">
-            <img src="https://img.freepik.com/free-vector/happy-freelancer-with-computer-home-young-man-sitting-armchair-using-laptop-chatting-online-smiling-vector-illustration-distance-work-online-learning-freelance_74855-8401.jpg?w=900&t=st=1667037491~exp=1667038091~hmac=7c71ea8afc8f3cc8065c5ccc05d105e3c8a7b76f0133016cb210a7882dc19611" className="img-fluid" alt="..."/>
+              <img src="https://img.freepik.com/free-vector/happy-freelancer-with-computer-home-young-man-sitting-armchair-using-laptop-chatting-online-smiling-vector-illustration-distance-work-online-learning-freelance_74855-8401.jpg?w=900&t=st=1667037491~exp=1667038091~hmac=7c71ea8afc8f3cc8065c5ccc05d105e3c8a7b76f0133016cb210a7882dc19611" className="img-fluid" alt="..." />
             </div>
           </div>
           <div className="col">
             <div className="px-5 py-5 mt-5 text-center">
               <div className="px-2 py-2 align-middle">
-              <h1>Alquiler de accesorio para eventos</h1>
+                <h1>Alquiler de accesorios para eventos</h1>
               </div>
               <div className="px-2 py-2">
-                <button type="button" className="btn btn-outline-primary">Resevar Ahora</button>
+                <button type="button" className="btn btn-outline-primary">Reservar Ahora</button>
               </div>
             </div>
           </div>
-      </div>
+        </div>
       </div>
       <div className="main-container">
         <div className="container-fluid">
+        </div>
       </div>
-      </div>
-      </div>
-    )
+    </div>
+  )
 }
 
 
