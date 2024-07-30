1157d25d171e542cbee44b0c96410c4d50c72711
NahuelPicca <nahuelpicca@gmail.com>
2024-07-30 00:29:09 -0300

    Fix: se actualizó la landingPage.

--- RossiEventosFront/src/LandingPage/LandingPage.tsx
+++ RossiEventosFront/src/LandingPage/LandingPage.tsx
@@ -1,32 +1,22 @@
 export function LandingPage() {
   return (
-    <div className="hero-container" id="hero-sec">
-      <div className="container-fluid ">
-        <div className="row d-flex">
-          <div className="col align-middle">
-            <div className="px-2 py-2">
-              <img src="https://img.freepik.com/free-vector/happy-freelancer-with-computer-home-young-man-sitting-armchair-using-laptop-chatting-online-smiling-vector-illustration-distance-work-online-learning-freelance_74855-8401.jpg?w=900&t=st=1667037491~exp=1667038091~hmac=7c71ea8afc8f3cc8065c5ccc05d105e3c8a7b76f0133016cb210a7882dc19611" className="img-fluid" alt="..." />
-            </div>
-          </div>
+
+    <div className="background-images">
+      <div className="nahuel"></div>
+      <div className="container2 text-center contendorpadre">
+        <div className="row">
           <div className="col">
-            <div className="px-5 py-5 mt-5 text-center">
-              <div className="px-2 py-2 align-middle">
-                <h1>Alquiler de accesorios para eventos</h1>
-              </div>
-              <div className="px-2 py-2">
-                <button type="button" className="btn btn-outline-primary">Reservar Ahora</button>
-              </div>
-            </div>
+            <h1 className="titulo mb-3">Alquiler de accesorios para <br></br>eventos</h1>
+            <button type="button" className="boton-violeta">Reservar Ahora</button>
           </div>
         </div>
       </div>
-      <div className="main-container">
-        <div className="container-fluid">
-        </div>
+      <div className="background-image-container left">
+        <div className="background-image left"></div>
+      </div>
+      <div className="background-image-container right">
+        <div className="background-image right"></div>
       </div>
     </div>
   )
 }
-
-
-
