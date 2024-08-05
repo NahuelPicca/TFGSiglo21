f31344cd3326b0a0ceb498498cae4f1c03c51e92
NahuelPicca <nahuelpicca@gmail.com>
2024-08-04 22:20:29 -0300

    Fix: se descomento el tema del jwt.

--- RossiEventosFront/src/auth/Login.tsx
+++ RossiEventosFront/src/auth/Login.tsx
@@ -2,23 +2,23 @@ import axios from "axios";
 import { urlUsuario } from "../utils/endpoints";
 import FormularioAuth from "./FormularoAuth";
 import { credencialesUsuario } from "./auth.model";
-import { useState } from "react";
+import { useContext, useState } from "react";
 import MostrarErrores from "../utils/MostrarErrores";
-//import { guardarTokenLocalStorage, obtenerClaims } from "./manejadorJWT";
-//import AutenticacionContext from "./AutenticacionContext";
 import { useNavigate } from "react-router-dom";
+import { guardarTokenLocalStorage, obtenerClaims } from "./manejadorJWT";
+import AutenticacionContext from "./AutenticacionContext";
 
 export default function Login() {
-    //const { actualizar } = useContext(AutenticacionContext);
+    const { actualizar } = useContext(AutenticacionContext);
     const [errores, setErrores] = useState<string[]>([]);
     const navigate = useNavigate();
     async function login(credenciales: credencialesUsuario) {
         try {
             const resultado = await axios.post(`${urlUsuario}/login`, credenciales)
-            //guardarTokenLocalStorage(resultado.data);
-            //actualizar(obtenerClaims());
+            guardarTokenLocalStorage(resultado.data);
+            actualizar(obtenerClaims());
             console.log(resultado);
-            navigate("/");
+            navigate("/producto");
         } catch (error: any) {
             setErrores(error.response.data)
         }
