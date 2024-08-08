132768d20e789f0dd8b22feed035b9e4caea5662
NahuelPicca <nahuelpicca@gmail.com>
2024-08-08 01:10:49 -0300

    Fix: se modificó otro DTO.

--- RossiEventos/RossiEventos/Dto/CUAsignacionVehicTranspDto.cs
+++ RossiEventos/RossiEventos/Dto/CUAsignacionVehicTranspDto.cs
@@ -2,8 +2,13 @@
 {
     public class CUAsignacionVehicTranspDto
     {
+        public int Id { get; set; }
+        public string Patente { get; set; }
+        public string Modelo { get; set; }
+        public string NombreTransportista { get; set; }
+        public string ApellidoTransportista { get; set; }
+        public string Licencia { get; set; }
         public int TransportitaId { get; set; }
         public int VehiculoId { get; set; }
-        public string UsuarioInserto { get; set; }
     }
 }
